cf042bd6c88c1d11bf92859487d7767a508360d3
achilles <779239635@qq.com>
2019-12-23 17:28:53 +0800

    finish base scroll

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { GlobalStyle } from  './style';
+import { GlobalStyle } from './style';
+import { IconStyle } from './assets/iconfont/iconfont';
+import routes from './routes/index'
+import { renderRoutes } from 'react-router-config'
+import { HashRouter } from 'react-router-dom'
+import store from './store/index'
+import { Provider } from 'react-redux'
 function App() {
   return (
-    <div>
-     
-    </div>
+    <Provider store={store}>
+      <HashRouter>
+        <GlobalStyle></GlobalStyle>
+        <IconStyle></IconStyle>
+        {renderRoutes(routes)}
+      </HashRouter>
+    </Provider>
   );
 }
 
