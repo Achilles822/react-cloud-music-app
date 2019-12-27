fe99a434777228c053e6b2a498a18eab1ac2f15b
achilles <779239635@qq.com>
2019-12-27 17:50:19 +0800

    [mod]

--- src/application/Singers/index.js
+++ src/application/Singers/index.js
@@ -1,8 +1,150 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react';
+import Horizen from '../../baseUI/horizen-item';
+import { categoryTypes, alphaTypes } from '../../api/config';
+import {
+    NavContainer,
+    ListContainer,
+    List,
+    ListItem
+} from "./style";
+import Scroll from '../../baseUI/Scroll'
+import {
+    getSingerList,
+    getHotSingerList,
+    changeEnterLoading,
+    changePageCount,
+    refreshMoreSingerList,
+    changePullUpLoading,
+    changePullDownLoading,
+    refreshMoreHotSingerList
+} from './store/actionCreators';
+import { connect } from 'react-redux';
+import LazyLoad, { forceCheck } from 'react-lazyload';
+import Loading from '../../baseUI/Loading';
 function Singers(props) {
+    let [category, setCategory] = useState('');
+    let [alpha, setAlpha] = useState('');
+
+    const { singerList, enterLoading, pullUpLoading, pullDownLoading, pageCount } = props;
+
+    const { getHotSingerDispatch, updateDispatch, pullDownRefreshDispatch, pullUpRefreshDispatch } = props;
+
+    useEffect(() => {
+        getHotSingerDispatch();
+        // eslint-disable-next-line
+    }, []);
+
+    let handleUpdateAlpha = (val) => {
+        setAlpha(val);
+        updateDispatch(category, val);
+    };
+
+    let handleUpdateCatetory = (val) => {
+        setCategory(val);
+        updateDispatch(val, alpha);
+    };
+
+    const handlePullUp = () => {
+        pullUpRefreshDispatch(category, alpha, category === '', pageCount);
+    };
+
+    const handlePullDown = () => {
+        pullDownRefreshDispatch(category, alpha);
+    };
+
+    // const singerList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(item => {
+    //     return {
+    //         picUrl: "https://p2.music.126.net/uTwOm8AEFFX_BYHvfvFcmQ==/109951164232057952.jpg",
+    //         name: "隔壁老樊",
+    //         accountId: 277313426,
+    //     }
+    // });
+
+    // 渲染函数，返回歌手列表
+    const renderSingerList = () => {
+        return (
+            <List>
+                {
+                    singerList.map((item, index) => {
+                        return (
+                            <ListItem key={item.accountId + "" + index}>
+                                <div className="img_wrapper">
+                                    <img src={`${item.picUrl}?param=300x300`} width="100%" height="100%" alt="music" />
+                                </div>
+                                <span className="name">{item.name}</span>
+                            </ListItem>
+                        )
+                    })
+                }
+            </List>
+        )
+    };
     return (
-        <div>Singers</div>
+        <div>
+            <NavContainer>
+                <Horizen
+                    list={categoryTypes}
+                    title={"分类 (默认热门):"}
+                    handleClick={(val) => handleUpdateCatetory(val)}
+                    oldVal={category}></Horizen>
+                <Horizen
+                    list={alphaTypes}
+                    title={"首字母:"}
+                    handleClick={val => handleUpdateAlpha(val)}
+                    oldVal={alpha}></Horizen>
+            </NavContainer>
+            <ListContainer>
+                <Scroll
+                    pullUp={handlePullUp}
+                    pullDown={handlePullDown}
+                    pullUpLoading={pullUpLoading}
+                    pullDownLoading={pullDownLoading}
+                >
+                    {renderSingerList()}
+                </Scroll>
+            </ListContainer>
+        </div>
     )
 }
-export default React.memo(Singers)
+
+const mapStateToProps = (state) => ({
+    singerList: state.getIn(['singers', 'singerList']),
+    enterLoading: state.getIn(['singers', 'enterLoading']),
+    pullUpLoading: state.getIn(['singers', 'pullUpLoading']),
+    pullDownLoading: state.getIn(['singers', 'pullDownLoading']),
+    pageCount: state.getIn(['singers', 'pageCount'])
+})
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        getHotSingerDispatch() {
+            dispatch(getHotSingerList())
+        },
+        updateDispatch(category, alpha) {
+            dispatch(changePageCount(0));//由于改变了分类，所以pageCount清零
+            dispatch(changeEnterLoading(true));//loading，现在实现控制逻辑，效果实现放到下一节，后面的loading同理
+            dispatch(getSingerList(category, alpha));
+        },
+        // 滑到最底部刷新部分的处理
+        pullUpRefreshDispatch(category, alpha, hot, count) {
+            dispatch(changePullUpLoading(true));
+            dispatch(changePageCount(count + 1));
+            if (hot) {
+                dispatch(refreshMoreHotSingerList());
+            } else {
+                dispatch(refreshMoreSingerList(category, alpha));
+            }
+        },
+        //顶部下拉刷新
+        pullDownRefreshDispatch(category, alpha) {
+            dispatch(changePullDownLoading(true));
+            dispatch(changePageCount(0));//属于重新获取数据
+            if (category === '' && alpha === '') {
+                dispatch(getHotSingerList());
+            } else {
+                dispatch(getSingerList(category, alpha));
+            }
+        }
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Singers);
--- src/baseUI/Scroll/index.js
+++ src/baseUI/Scroll/index.js
@@ -2,17 +2,40 @@ import React, { useState, useEffect, useMemo, useRef, forwardRef, useImperativeH
 import PropTypes from 'prop-types'
 import BScroll from "better-scroll"
 import styled from 'styled-components'
+import LoadingV2 from '../loading-v2/index';
+import Loading from '../Loading/index'
 // import { debounce } from '../../api/utils'
 const ScrollContainer = styled.div`
   width: 100%;
   height: 100%;
   overflow: hidden;
 `
+const PullUpLoading = styled.div`
+  position: absolute;
+  left:0; right:0;
+  bottom: 5px;
+  width: 60px;
+  height: 60px;
+  margin: auto;
+  z-index: 100;
+`;
+
+export const PullDownLoading = styled.div`
+  position: absolute;
+  left:0; right:0;
+  top: 0px;
+  height: 30px;
+  margin: auto;
+  z-index: 100;
+`;
+
 const Scroll = forwardRef((props, ref) => {
     const [bScroll, setBScroll] = useState()
     const scrollContainerRef = useRef()
     const { direction, click, refresh, pullUpLoading, pullDownLoading, bounceTop, bounceBottom } = props;
     const { pullUp, pullDown, onScroll } = props
+
+
     useEffect(() => {
         const scroll = new BScroll(scrollContainerRef.current, {
             scrollX: direction === "horizental",
@@ -82,11 +105,17 @@ const Scroll = forwardRef((props, ref) => {
             }
         }
     }))
+    const PullUpdisplayStyle = pullUpLoading ? { display: "" } : { display: "none" };
+    const PullDowndisplayStyle = pullDownLoading ? { display: "" } : { display: "none" };
     return (
         <ScrollContainer ref={scrollContainerRef}>
-          {props.children}
+            {props.children}
+            {/* 滑到底部加载动画 */}
+            <PullUpLoading style={PullUpdisplayStyle}><Loading></Loading></PullUpLoading>
+            {/* 顶部下拉刷新动画 */}
+            <PullDownLoading style={PullDowndisplayStyle}><LoadingV2></LoadingV2></PullDownLoading>
         </ScrollContainer>
-      );
+    );
 })
 
 
